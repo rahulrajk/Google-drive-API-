d91e3caf63ffd42c8f1e470d5c37171b32d638d0
Rahul K <rahul.raj@singlularium.in>
2019-12-19 15:58:13 +0530

    Codes

---
+++ quickstart.py
@@ -0,0 +1,107 @@
+from __future__ import print_function
+import pickle
+import os.path,io
+from googleapiclient.discovery import build
+from google_auth_oauthlib.flow import InstalledAppFlow
+from google.auth.transport.requests import Request
+from apiclient.http import MediaFileUpload,MediaIoBaseDownload
+
+
+# If modifying these scopes, delete the file token.pickle.
+SCOPES = ['https://www.googleapis.com/auth/drive']
+
+# Function for uploading file
+def uploadFile(filename,filepath,creds):
+    drive_service = build('drive', 'v3', credentials=creds)
+    file_metadata = {'name':filename}
+    media = MediaFileUpload(filepath,
+                            mimetype='image/jpeg')
+    file = drive_service.files().create(body=file_metadata,
+                                        media_body=media,
+                                        fields='id').execute()
+    print('File ID: %s' % file.get('id'))
+
+
+# Function for downloading file
+def downloadFile(file_id,filepath,drive_service):
+    request = drive_service.files().get_media(fileId=file_id)
+    fh = io.BytesIO()
+    downloader = MediaIoBaseDownload(fh, request)
+    done = False
+    while done is False:
+        status, done = downloader.next_chunk()
+        print("Download %d%%." % int(status.progress() * 100))
+    with io.open(filepath,'wb') as f:
+        fh.seek(0)
+        f.write(fh.read())
+
+
+# Function for creating file
+def createFolder(name,drive_service):
+    file_metadata = {
+    'name': name,
+    'mimeType': 'application/vnd.google-apps.folder'
+    }
+    file = drive_service.files().create(body=file_metadata,
+                                        fields='id').execute()
+    print('Folder ID: %s' % file.get('id'))
+
+
+# Function for searching file
+def searchFile(size,drive_service,query):
+    results = drive_service.files().list(
+        pageSize=size, fields="nextPageToken, files(id, name)",q=query).execute()
+    items = results.get('files', [])
+    if not items:
+        print('No files found.')
+    else:
+        print('Files:')
+        for item in items:
+            print(u'{0} ({1})'.format(item['name'], item['id']))    
+
+
+def main():
+    """Shows basic usage of the Drive v3 API.
+    Prints the names and ids of the first 10 files the user has access to.
+    """
+    creds = None
+    # The file token.pickle stores the user's access and refresh tokens, and is
+    # created automatically when the authorization flow completes for the first
+    # time.
+    if os.path.exists('token.pickle'):
+        with open('token.pickle', 'rb') as token:
+            creds = pickle.load(token)
+    # If there are no (valid) credentials available, let the user log in.
+    if not creds or not creds.valid:
+        if creds and creds.expired and creds.refresh_token:
+            creds.refresh(Request())
+        else:
+            flow = InstalledAppFlow.from_client_secrets_file(
+                'credentials.json', SCOPES)
+            creds = flow.run_local_server(port=0)
+        # Save the credentials for the next run
+        with open('token.pickle', 'wb') as token:
+            pickle.dump(creds, token)
+    drive_service = build('drive', 'v3', credentials=creds)
+
+    # Call the Drive v3 API
+    results = drive_service.files().list(
+        pageSize=10, fields="nextPageToken, files(id, name)").execute()
+    items = results.get('files', [])
+    if not items:
+        print('No files found.')
+    else:
+        print('Files:')
+        for item in items:
+            print(u'{0} ({1})'.format(item['name'], item['id']))
+
+
+    #Methods for accessing Google Drive
+    uploadFile('google.jpg','google.jpg',creds)
+    downloadFile('1HGFBLpHgWJKPuTAgDfFh40dNakqy1cQF','imgdownloadedfromdrive.jpg',drive_service)
+    createFolder('driveapi',drive_service)
+    searchFile(10,drive_service,"name contains 'Google'")
+
+
+if __name__ == '__main__':
+    main()
